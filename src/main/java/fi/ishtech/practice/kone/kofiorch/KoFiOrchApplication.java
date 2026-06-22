46fc2e283604de96c601cbe3374c699860a9b432
Muneer Ahmed Syed <muneer@ishtech.fi>
2026-06-22 17:29:50 +0300

    Initial code

---
+++ src/main/java/fi/ishtech/practice/kone/kofiorch/KoFiOrchApplication.java
@@ -0,0 +1,13 @@
+package fi.ishtech.practice.kone.kofiorch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoFiOrchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KoFiOrchApplication.class, args);
+	}
+
+}
